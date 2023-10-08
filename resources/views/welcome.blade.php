3623053dcdae4048489aff7a5ef394604b6c1069
Alnil Tanudtanud <56903193+spade619@users.noreply.github.com>
2023-10-08 15:19:28 +0800

    added image and css styling folder

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,7 +2,10 @@
     @extends('layouts.layout')
 
     @section('content')
-      <div class="text-center">welcome page</div>
+      <div class="text-center">
+        <img src="/img/pizzatest.jpg" alt="this is pizza logo">
+        <h1 class="title">welcome to nelmi pizza</h1>
+      </div>
 
    
 
